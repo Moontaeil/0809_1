390e5a8ff4823cdd8a61d966cb513c21d74d358b
Moon <xodlf39@gmail.com>
2024-08-09 15:07:21 +0900

    커밋완료

---
+++ Project_Board_EX/src/main/java/kr/co/softsoldesk/DAO/BoardDAO.java
@@ -0,0 +1,54 @@
+package kr.co.softsoldesk.DAO;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import kr.co.softsoldesk.beans.ContentBean;
+import kr.co.softsoldesk.mapper.BoardMapper;
+
+@Repository
+public class BoardDAO {
+
+	@Autowired
+	private BoardMapper boardMapper;
+	
+	//게시글 저장
+	public void addContentInfo(ContentBean writeContentBean) {
+		
+		boardMapper.addContentInfo(writeContentBean);
+		
+	}
+	
+	//게시판 이름 가져오기
+	public String getBoardInfoName(int board_info_idx) {
+		
+		return boardMapper.getBoardInfoName(board_info_idx);
+	}
+	
+	//특정 게시판의 게시글 제목, 작성자 이름, 작성날짜 가져오기
+	public List<ContentBean> getContentList(int board_info_idx){
+		
+		return boardMapper.getContentList(board_info_idx);
+	}
+	
+	//특정 게시글 가져오기
+	public ContentBean getContentInfo(int content_idx) {
+		
+		return boardMapper.getContentInfo(content_idx);
+	}
+	
+	//게시글 업데이트
+	public void modifyContentInfo(ContentBean modifyContentBean) {
+		
+		boardMapper.modifyContentInfo(modifyContentBean);
+	}
+	
+	//게시글 삭제
+	public void deleteContentInfo(int content_idx) {
+		
+		boardMapper.deleteContentInfo(content_idx);
+	}
+	
+}
---
+++ Project_Board_EX/src/main/java/kr/co/softsoldesk/controller/MainController.java
@@ -0,0 +1,21 @@
+package kr.co.softsoldesk.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import kr.co.softsoldesk.beans.BoardInfoBean;
+import kr.co.softsoldesk.service.TopMenuService;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/main")
+	private String main() {
+	
+		return "main";
+	}
+}
